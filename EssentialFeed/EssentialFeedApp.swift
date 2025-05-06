a7743abfacbcec2e2810105e63159914ed2d0227
Kusum Miraj <kusum.miraj@gmail.com>
2025-05-06 14:05:07 -0400

    Initial Commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Kusum Miraj on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
