91f29492317a0c251b8d6253d97f12a2e00fc40e
Kusum Miraj <kusum.miraj@gmail.com>
2025-05-13 15:46:18 -0400

    Use explicit json data for non 200 status code response data

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -47,7 +47,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
         
         codeSamples.enumerated().forEach { index, code in
             expect(sut, toCompleteWithError: .failure(.invalidData)) {
-                client.complete(withStatusCode: code, at: index)
+                let json = makeItemsJSON([])
+                client.complete(withStatusCode: code, data: json, at: index)
             }
         }
     }
@@ -142,7 +143,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
             guard let response = HTTPURLResponse(
                url: requestedURLs[index],
                statusCode: code,
