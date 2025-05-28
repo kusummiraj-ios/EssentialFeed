0d616dde5f88daa43fe3d99e0af8c73246b58dce
Kusum Miraj <kusum.miraj@gmail.com>
2025-05-28 12:04:22 -0400

    Removes reduntant test

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -20,15 +20,6 @@ class URLSessionHTTPClient {
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
-
-    func test_getFromURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPClient(session: session)
-        
-        sut.get(from: url)
-        XCTAssertEqual(session.receivedURLs, [url])
-    }
     
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
@@ -49,14 +40,12 @@ private class FakeURLSessionDataTask: URLSessionDataTask {
     override func resume() { }
 }
 private class URLSessionSpy: URLSession {
-    var receivedURLs = [URL]()
     private var stubs = [URL: URLSessionDataTask]()
     func stub(url: URL, task: URLSessionDataTask) {
         stubs[url] = task
     }
     
     override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        receivedURLs.append(url)
         return stubs[url] ?? FakeURLSessionDataTask()
     }
 }
